63845830db499bcf32dc69ca3aedeb7e3586932e
Robert Szczygielski <szczygielski@dicedev.pl>
2024-06-28 10:32:08 +0200

    init gitignore

---
+++ src/main/java/pl/dicedev/pethotel/PethotelApplication.java
@@ -0,0 +1,13 @@
+package pl.dicedev.pethotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PethotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PethotelApplication.class, args);
+	}
+
+}
