5374785398316d73633461afc191aab89d9d4e23
Robert Szczygielski <szczygielski@dicedev.pl>
2024-08-23 19:57:25 +0200

    add room type

--- src/main/java/pl/dicedev/pethotel/PethotelApplication.java
+++ src/main/java/pl/dicedev/pethotel/PethotelApplication.java
@@ -2,11 +2,14 @@ package pl.dicedev.pethotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import pl.dicedev.pethotel.config.RoomsTypeProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RoomsTypeProperty.class)
 @Configuration
 public class PethotelApplication {
 
--- src/test/java/pl/dicedev/pethotel/PethotelApplicationTests.java
+++ src/test/java/pl/dicedev/pethotel/PethotelApplicationTests.java
@@ -3,7 +3,6 @@ package pl.dicedev.pethotel;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PethotelApplicationTests {
 
 	@Test
